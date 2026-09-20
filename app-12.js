6526f59fdf8d907dfeee943ed5d7e190a814803d
ziboamos7-web <zibo.amos7@gmail.com>
2026-09-20 23:26:07 +0000

    Delete app-12.js

--- app-12.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  window.dashboardQuickGo=function(route){
-    try{ Router.go(String(route)); }
-    catch(e){ console.error('Quick navigation',e); if(window.Toast&&Toast.show) Toast.show('Impossible d’ouvrir cette rubrique'); }
-  };
-})();
