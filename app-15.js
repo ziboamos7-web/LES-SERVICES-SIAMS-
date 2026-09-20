91994dcb7ad055742a0f2fdb591859b46eed3765
ziboamos7-web <zibo.amos7@gmail.com>
2026-09-20 23:27:00 +0000

    Delete app-15.js

--- app-15.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  const originalGo=Router.go.bind(Router);
-  Router.go=function(name,opts){
-    if(name==='register' && !ActivationGate.getStored()) name='activation-code';
-    if(name==='login') authMode='login';
-    return originalGo(name,opts);
-  };
-})();
